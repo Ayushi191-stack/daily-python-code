2d855a5f739680854aedac646b161c1f5e058e66
ayushi25bsa10121-ui <ayushi.25bsa10121@vitbhopal.ac.in>
2026-05-28 15:52:14 +0530

    completed Day 3

---
+++ CHAPTER 8/function.py
@@ -0,0 +1,15 @@
+def avg(): ##function definition
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    c = int(input("Enter the third number: "))
+    average = (a + b + c) / 3
+    print("The average is:", average)
+
+avg()  #function call
+
+#greet a person by def function
+def goodday():
+    print("Good day!")
+
+goodday()
+
---
+++ CHAPTER 8/functuinWithArgument.py
@@ -0,0 +1,11 @@
+# def goodday(name):  #function definition with argument
+#     print("Good day, " + name + "!")
+
+# goodday("Ayushi")  #function call with argument
+
+def goodday(name):  #function definition with argument
+    print("Good day, " + name + "!")
+    return "done"
+
+a = goodday("Ayushi") 
+print(a)
